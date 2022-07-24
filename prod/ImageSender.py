133006858ea5ed7a52ae7fb74d03b58b034c8124
Sean <conk3r_xd@hotmail.com>
2022-07-24 14:41:20 +0200

    fixed content for discord

--- prod/ImageSender.py
+++ prod/ImageSender.py
@@ -1,11 +1,18 @@
+import json
 import os
 import requests
 
-class ImageSender: 
-    def SendImage(path_img, url):
+
+class ImageSender:
+    def SendImage(path_img, url, title):
+        postData = {
+            "content": title
+        }
+
         with open(path_img, 'rb') as img:
-            name_img= os.path.basename(path_img)
-            files= {'image': (name_img,img,'multipart/form-data',{'Expires': '0'}) }
+            name_img = os.path.basename(path_img)
+            files = {
+                'image': (name_img, img, 'multipart/form-data', {'Expires': '0'})}
             with requests.Session() as s:
-                r = s.post(url,files=files)
+                r = s.post(url, files=files, data=postData)
                 print(r.status_code)
--- prod/main.py
+++ prod/main.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 from matplotlib import dates
 
-#my classes
+# my classes
 from calc import CalcMean
 from fetcher import fetcher
 from ImageSender import ImageSender
@@ -34,9 +34,10 @@ async def StartStockApp():
             #
             if result:
                 shortName = ticker.info['shortName']
-                axs = histClose.plot( figsize=(15, 7), title=symbol+' | '+shortName)
+                axs = histClose.plot(figsize=(15, 7), title=symbol+' | '+shortName)
                 roundedLastClose = round(histClose.iloc[-1], 2)
-                axs.set_title(symbol+' | '+shortName+' | '+str(roundedLastClose))
+                title = symbol+' | '+shortName + ' | '+str(roundedLastClose)
+                axs.set_title(title)
                 axs.xaxis.set_major_locator(dates.MonthLocator(interval=4))
                 plt.axvline(['2020-02-21'], linestyle='dotted', color='tab:red')
                 plt.tight_layout()
@@ -45,11 +46,11 @@ async def StartStockApp():
                 folder = 'pics'
                 filename = symbol+'_' + str(now) + '_saved_figure-50pi.png'
                 imagePath = folder+'/'+filename
-                plt.savefig(imagePath, dpi = 50)
+                plt.savefig(imagePath, dpi=50)
                 plt.close()  # or fig.clear()
-                ImageSender.SendImage(imagePath, os.getenv('DISCORD_URL'))
+                if(os.getenv('SEND_TO_DISCORD')):
+                    ImageSender.SendImage(imagePath, os.getenv('DISCORD_URL'), title)
             else:
                 plt.close()  # or fig.clear()
 
 asyncio.run(StartStockApp())
-
