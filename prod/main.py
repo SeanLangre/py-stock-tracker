f7e211aebe808ab43c112754e9461d6abf2b4322
Sean <conk3r_xd@hotmail.com>
2023-04-10 23:03:53 +0200

    2023 fixes

--- prod/main.py
+++ prod/main.py
@@ -46,58 +46,56 @@ async def StartStockApp():
             print("yes! the var is null")
 
         ticker = stock[0]
-        if not "shortName" in ticker.info:
-            print("shortName is null")
+        info = ticker.info
+        histOpen = stock[1]["Open"]
+        histHigh = stock[1]["High"]
+        histLow = stock[1]["Low"]
+        histClose = stock[1]["Close"]
+        # symbol = info["symbol"]
+        # print("{} ({})".format(symbol, x))
+        result = CalcMean.MeanHigherThanCurrent(ticker)
+        #
+        if result:
+            shortName = ticker.ticker
+            axs = histClose.plot(figsize=(15, 7), title=" | " + shortName)
+            # axs = histClose.plot(figsize=(15, 7), title=symbol + " | " + shortName)
+            roundedLastOpen = round(histOpen.iloc[-1], 2)
+            roundedLastHigh = round(histHigh.iloc[-1], 2)
+            roundedLastLow = round(histLow.iloc[-1], 2)
+            roundedLastClose = round(histClose.iloc[-1], 2)
+            title = (
+                ""
+                + GetIsBiggerSymbol(roundedLastOpen, roundedLastClose)
+                + " "
+                + GetDailyDiff(roundedLastOpen, roundedLastClose)
+                + " "
+                + " | "
+                + shortName
+                + " | Op: "
+                + str(roundedLastOpen)
+                + " Hi: "
+                + str(roundedLastHigh)
+                + " Lo: "
+                + str(roundedLastLow)
+                + " Cl: "
+                + str(roundedLastClose)
+            )
+            axs.set_title(title)
+            axs.xaxis.set_major_locator(dates.MonthLocator(interval=4))
+            plt.axvline(["2020-02-21"], linestyle="dotted", color="tab:red")
+            plt.tight_layout()
+            # plt.show()
+            now = time.strftime("%Y%m%d-%H%M%S")
+            folder = "pics"
+            filename = shortName + "_" + str(now) + "_saved_figure-50pi.png"
+            imagePath = folder + "/" + filename
+            plt.savefig(imagePath, dpi=50)
+            plt.close()  # or fig.clear()
+            sendToDiscord = os.getenv("SEND_TO_DISCORD")
+            if sendToDiscord == "True":
+                ImageSender.SendImage(imagePath, os.getenv("DISCORD_URL"), title)
         else:
-            histOpen = stock[1]["Open"]
-            histHigh = stock[1]["High"]
-            histLow = stock[1]["Low"]
-            histClose = stock[1]["Close"]
-            symbol = ticker.info["symbol"]
-            print("{} ({})".format(symbol, x))
-            result = CalcMean.MeanHigherThanCurrent(ticker)
-            #
-            if result:
-                shortName = ticker.info["shortName"]
-                axs = histClose.plot(figsize=(15, 7), title=symbol + " | " + shortName)
-                roundedLastOpen = round(histOpen.iloc[-1], 2)
-                roundedLastHigh = round(histHigh.iloc[-1], 2)
-                roundedLastLow = round(histLow.iloc[-1], 2)
-                roundedLastClose = round(histClose.iloc[-1], 2)
-                title = (
-                    ""
-                    + GetIsBiggerSymbol(roundedLastOpen, roundedLastClose)
-                    + " "
-                    + GetDailyDiff(roundedLastOpen, roundedLastClose)
-                    + " "
-                    + symbol
-                    + " | "
-                    + shortName
-                    + " | Op: "
-                    + str(roundedLastOpen)
-                    + " Hi: "
-                    + str(roundedLastHigh)
-                    + " Lo: "
-                    + str(roundedLastLow)
-                    + " Cl: "
-                    + str(roundedLastClose)
-                )
-                axs.set_title(title)
-                axs.xaxis.set_major_locator(dates.MonthLocator(interval=4))
-                plt.axvline(["2020-02-21"], linestyle="dotted", color="tab:red")
-                plt.tight_layout()
-                # plt.show()
-                now = time.strftime("%Y%m%d-%H%M%S")
-                folder = "pics"
-                filename = symbol + "_" + str(now) + "_saved_figure-50pi.png"
-                imagePath = folder + "/" + filename
-                plt.savefig(imagePath, dpi=50)
-                plt.close()  # or fig.clear()
-                sendToDiscord = os.getenv("SEND_TO_DISCORD")
-                if sendToDiscord == "True":
-                    ImageSender.SendImage(imagePath, os.getenv("DISCORD_URL"), title)
-            else:
-                plt.close()  # or fig.clear()
+            plt.close()  # or fig.clear()
     if os.getenv("REMOVE_PICS") == "True":
         # remove dir
         shutil.rmtree(folder, onerror=remove_readonly)
